49fa4f83ffc359a15d06b4f74bf235b38052974d
Yeasin <arafat.ya19@gmail.com>
2021-10-18 17:12:37 +0600

    hello world

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 20,
+        title: Text("HomePage"),
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: LinearProgressIndicator(
+          backgroundColor: Colors.redAccent,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Demo App",
+    home:  new HomePage(),
+  ));
+
+}
